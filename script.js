5f101087fc5c9e6ef603cc152b69d4f69b1b2704
cpa9almeida <133358082+cpa9almeida@users.noreply.github.com>
2023-07-07 11:43:36 +0000

    nova versao

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function carregar() {
-    var msg = window.document.getElementsByld('msg')
-    var img = window.documents.getElementsByld('foto')
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('foto')
     var data = new Date()
     var hora = data.getHours();
-    msg.innerHTML = `Agora são $(hora) horas.`
+    hora = 16
+    msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
-        img.src = 'fotomanha(1).png'
-        documents.body.style.background = '#e2cd9f'
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#e2cd9f'
     } else if(hora >= 12 && hora <= 18) {
         // BOA TARDE!
-        img.scr = 'fototarde(1).png'
-        documents.bod.style.background = '#b9846f'
+        img.src = 'fototarde.png'
+        document.body.style.background = '#b9846f'
     } else {
         // BOA NOITE!
-        img.scr = 'fotonoite(1).png'
-        documents.bod.style.background = '#515154'
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#515154'
 
     }
 }
