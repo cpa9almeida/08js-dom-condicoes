b8dfd12537f2082d2b06ec0a88176501ae5eb864
cpa9almeida <133358082+cpa9almeida@users.noreply.github.com>
2023-07-08 02:56:19 +0000

    oi

--- scriptidade.js
+++ scriptidade.js
@@ -15,10 +15,35 @@ function verificar() {
             gênero = 'Homem'
             if (idade >=0 && idade < 10) {
                 //criança 
+                img.setAttribute('src','bebeMenino.jpeg')
+            } else if (idade < 21) { 
+                //jovem
+                img.setAttribute('src','adolescenteHomen.jpeg')
+            } else if (idade < 50) {
+                //adulto 
+                img.setAttribute('src','homemAdulto.jpeg')
+            } else {
+                //idoso
+                img.setAttribute('src','homemIdoso.jpeg')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher' 
+            if (idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src','bebeMenina.jpeg')
             } else if (idade < 21) {
-                
+                //jovem
+                img.setAttribute('src','adolescenteMulher.jpeg')
+            } else if (idade < 50) {
+                //aduto
+                img.setAttribute('src','mulherAdulta.jpeg')
+            } else {
+                //idoso 
+                img.setAttribute('src','mulherIdosa.jpeg')
             }
         }
-    }
-
+        res.style.textAling = 'center'
+        res.innerHTML = `Detectamos $(gênero) com &(idade) anos.`
+        res.appendChild(img)
+    }   
 }
