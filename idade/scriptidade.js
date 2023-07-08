247fc67df904db6745310e9ea84b321f1264102b
cpa9almeida <133358082+cpa9almeida@users.noreply.github.com>
2023-07-08 19:12:47 +0000

    sw

--- idade/scriptidade.js
+++ idade/scriptidade.js
@@ -1,6 +1,6 @@
 function verificar() {
     var data = new Date()
-    var ano = new data.getFullyYear()
+    var ano = data.getFullyYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
     if (fano.value.length == 0 || Number(fano.value) > ano) {
